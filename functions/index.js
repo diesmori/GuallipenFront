185a33efca711fd0caf591f5cbbded37b40f2c1c
diesmori <diesmori@gmail.com>
2021-04-13 13:38:33 -0400

    Functions: check if truck has arrived

--- functions/index.js
+++ functions/index.js
@@ -40,6 +40,23 @@ function unixToDate(unix_timestamp) {
   return formattedTime;
 }
 
+async function getClientLatLon(id) {
+  return new Promise(resolve => {
+    admin
+      .database()
+      .ref("Clientes/" + id)
+      .once("value", snapshot => {
+        const results = snapshot.val();
+        if ("Latitud" in results)
+          resolve({ Latitud: results.Latitud, Longitud: results.Longitud });
+        else resolve(null);
+      });
+  }).catch(err => {
+    console.log(err);
+    resolve(null);
+  });
+}
+
 exports.presendEmail = functions.database
   .ref("Ordenes/{date}/{Id}")
   .onWrite((change, context) => {
@@ -81,4 +98,46 @@ exports.presendEmail = functions.database
             message: { subject: asunto, text: cuerpoMensaje }
           });
       });
+  });
+
+exports.checkLatLon = functions.database
+  .ref("Transportistas/{id}")
+  .onWrite((change, context) => {
+    const after = change.after.val(); // DataSnapshot after the change
+    const idTransportista = context.params.id;
+    // Obtener LatLon del transportista
+    const currentLat = after.Latitud;
+    const currentLon = after.Longitud;
+
+    // Buscar por cada cliente de la ruta
+    after.Ruta.Pedidos.map(async (pedido, index) => {
+      if ("Cliente" in pedido) {
+        console.log(pedido.Cliente);
+        // Si el cliente tiene LatLon, cotejar con la del transportista
+        const clientLatLon = await getClientLatLon(pedido.Cliente);
+        if (clientLatLon !== null) {
+          const clientLat = clientLatLon.Latitud;
+          const clientLon = clientLatLon.Longitud;
+          // Si está dentro del rango, postear con return para no seguir buscando
+          const rangoLat = 0.002; // 220m
+          const rangoLon = 0.003; // 270m
+          console.log(Math.abs(currentLat - clientLat));
+          console.log(Math.abs(currentLon - clientLon));
+          if (Math.abs(currentLat - clientLat) < rangoLat) {
+            if (Math.abs(currentLon - clientLon) < rangoLon) {
+              // El transportista está dentro del rango del cliente. Retornar
+              return admin
+                .database()
+                .ref(
+                  "Transportistas/" + idTransportista + "/Ruta/Pedidos/" + index
+                )
+                .update({
+                  haLlegado: true
+                });
+            }
+          }
+        }
+      }
+    });
+    return true;
   });
